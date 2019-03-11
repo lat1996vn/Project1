9ebba7cf7d71a38cf02527e45ea60fd29594eaf3
Lat <tuan.la2611@gmail.com>
2019-03-11 16:05:29 +0700

    hidesBarsOnTap and large titles

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UITableViewController {
     var picture = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "ImageView"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -32,6 +35,13 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = picture[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = picture[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 }
